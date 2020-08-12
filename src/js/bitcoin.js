26bc784975d7907313346a7a884a8e0fe19cea6b
ayoayomide <ayoayomide124@gmail.com>
2020-08-12 12:10:19 +0100

    new update

--- src/js/bitcoin.js
+++ src/js/bitcoin.js
@@ -2,19 +2,23 @@ Vue.component("btc-body", {
   props: {
     value: "",
   },
-  template: `<div>
-    <p>bitcoin</p>
+  template: `<div class="btc-wrap">
+    <p>Rate Converter</p>
+    <div class="right-wrap">
+    <p>Bitcoin</p>
     <input 
     v-bind:value="this.$parent.convert2btc"
     v-on:input="updateBtc({type : 'Dollar',value : $event.target.value})"
-    placeholder="enter bitcoin"
-    >
-    <p>dollar</p>
+    placeholder="Enter Bitcoin"
+    ><br>
+    <div class="calculate-wrap"><button></button><p>Calculate</p></div>
+    <p>Usd</p>
     <input
+    class="usd"
     v-bind:value="this.$parent.convert2dollar"
     v-on:input="updateBtc({type : 'Bitcoin',value : $event.target.value})"
-    placeholder="enter dollar"
-    >
+    placeholder="Enter Usd"
+    ></div>
     <p>we will pay : {{this.$parent.convert2dollar * this.$parent.companyRate}} naira</p>
     </div>
     `,
