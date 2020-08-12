7e90fe794533292ca75226a730685344f7c073e7
ayoayomide <ayoayomide124@gmail.com>
2020-08-12 15:43:22 +0100

    flip

--- src/js/bitcoin.js
+++ src/js/bitcoin.js
@@ -4,6 +4,14 @@ Vue.component("btc-body", {
   },
   template: `<div class="btc-wrap">
     <p>Rate Converter</p>
+    <div class="right-wrap">
+    <button class="convert-btn"></button>
+    <h3>Total Payout</h3>
+    <span>Rate : <big>#{{this.$parent.companyRate}}.00</big></span><br>
+    <p class="payout"># {{this.$parent.convert2dollar * this.$parent.companyRate}}.00</p><br>
+    <div class="bnk-label"><p class="left">Bank Deposit</p><p class="right">1000 NGN</p></div>
+    <p class="bnk-btc">1 BTC</p>
+    </div>
     <div class="left-wrap">
     <p>Bitcoin</p>
     <input 
@@ -19,14 +27,6 @@ Vue.component("btc-body", {
     v-on:input="updateBtc({type : 'Bitcoin',value : $event.target.value})"
     placeholder="Enter Usd"
     ></div>
-    <div class="right-wrap">
-    <button class="convert-btn"></button>
-    <h3>Total Payout</h3>
-    <span>Rate : <big>#{{this.$parent.companyRate}}.00</big></span><br>
-    <p class="payout"># {{this.$parent.convert2dollar * this.$parent.companyRate}}.00</p><br>
-    <div class="bnk-label"><p class="left">Bank Deposit</p><p class="right">1000 NGN</p></div>
-    <p class="bnk-btc">1 BTC</p>
-    </div>
     </div>
     `,
   methods: {
